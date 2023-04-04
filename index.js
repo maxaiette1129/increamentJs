8a8bc407ca3f631bd77085dfe6a0e90890bdf95f
Maxaiette <maxaiettec@gmail.com>
2023-04-04 09:25:55 +0800

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
-
+let countEl = document.getElementById("count-el")
 let count = 0
 
 function increament()
 {
     count = count + 1
-    document.getElementById("count-el").innerText = count
+    countEl.innerText = count
+    
 }
